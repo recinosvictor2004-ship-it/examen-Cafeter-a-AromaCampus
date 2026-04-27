52702c13533f6e33836acd16c0b17bcbbaf57204
LuisEstrada <luisestrada127016@gmail.com>
2026-04-27 15:34:55 -0600

    feat: :sparkles: Se agrego funcion para eliminar un cafe en especifico.
    En la funcion se solicita informacion al usuario para determinar que cafe sera eliminado

---
+++ inventario.py
@@ -0,0 +1,6 @@
+def eliminar_cafe(cafe_id):
+    coffees = load_coffees()
+    new_list = [c for c in coffees if c["id"] != cafe_id]
+    if len(new_list) == len(coffees):
+        raise ValueError("Café no encontrado.")
+    save_coffees(new_list)
